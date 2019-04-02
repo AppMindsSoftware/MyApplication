96eba4151be63fddf0a0695ec3a376e8f45a413a
rizesony <rizesony@gmail.com>
2019-04-02 12:05:13 +0530

    testcommit

--- app/src/main/java/chatapp/com/myapplication/MainActivity.java
+++ app/src/main/java/chatapp/com/myapplication/MainActivity.java
@@ -1,13 +1,37 @@
 package chatapp.com.myapplication;
 
+import android.content.Context;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import roboguice.inject.InjectView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.textView) TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        TextView textView = findViewById(R.id.textView);
+
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        String sensorInfo = "";
+        for (Sensor s : sensorList){
+            sensorInfo= sensorInfo + s.getName()+ "\n";
+        }
+        textView.setText(sensorInfo);
+
     }
 }
